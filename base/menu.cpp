26de8c787d2770617b4a58e18e4ab22f17ef3157
Joedahms <dahms.joejoe@gmail.com>
2022-12-19 16:53:45 -0600

    setting up abstract menu class

--- base/menu.cpp
+++ base/menu.cpp
@@ -1,1 +1,16 @@
+#include "menu.h"
 
+#include <vector>
+#include <string>
+
+std::string menu::get_disp_choices(int chc_idx) {
+	std::string temp_choice;
+	temp_choice = this.disp_choices.at(chc_idx);
+	return temp_choice;
+}
+
+void menu::set_disp_choices(int chc_idx, std::string new_str) {
+	this.disp_choices[chc_idx] = new_str;
+}	
+
+std::string menu::get_in_choices(
--- base/menu.h
+++ base/menu.h
@@ -1,6 +1,23 @@
 #ifndef MENU.H
 #define MENU.H
 
+#include <vector>
+#include <string>
+
+class menu {
+	private:
+		std::vector<std::string> disp_choices;
+		std::vector<std::string> in_choices;
+
+	public: 
+		virtual void fill_choices() = 0;
+		virtual void run_menu() = 0;
+
+		std::string get_disp_choices(int chc_idx);
+		void set_disp_choices(int chc_idx, std::string new_str);
+		std::string get_in_choices(int chc_idx);
+		void set_in_choices(int chc_idx, int new_int);
+}
 
 
 #endif
